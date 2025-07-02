cbb9fbcd0e63f2c9a39fe6b36ffaf12e7982fa8c
omar <omarenriquecs@gmail.com>
2025-07-02 19:26:12 -0400

    part1-14

--- part1/src/main.jsx
+++ part1/src/main.jsx
@@ -1,4 +1,5 @@
 import ReactDOM from "react-dom/client";
-import App from "./App.jsx";
+// import App from "./App.jsx";
+import App from "./App1-12"
 
 ReactDOM.createRoot(document.getElementById("root")).render(<App />);
